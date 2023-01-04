da13f9e3cabf2c8fef4def3af81e9a706e367554
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-04 23:13:58 +0530

    Deploy the React app to Github Pages

---
+++ precache-manifest.3f93c891b0be6d1a77ae6ef8d5ee7957.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34aeda6d8457f0a2bd24",
+    "url": "/Portfolio-React/static/css/main.183fbeee.chunk.css"
+  },
+  {
+    "revision": "34aeda6d8457f0a2bd24",
+    "url": "/Portfolio-React/static/js/main.34aeda6d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Portfolio-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/Portfolio-React/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "3b3d7fb1b6a578688a04",
+    "url": "/Portfolio-React/static/js/runtime~main.3b3d7fb1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Portfolio-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1799942f09167f02656e200d143aaa3a",
+    "url": "/Portfolio-React/index.html"
+  }
+];
