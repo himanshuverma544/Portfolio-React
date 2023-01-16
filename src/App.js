ccaad846dad51f903e45a874b5c6dec3f80dfd7c
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-16 15:30:45 +0530

    using all components and passing the required values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    "Going to create portfolio website."
-  );
-}
+import "./Assets";
+
+import Navigation from "./Navigation";
+import Masthead from "./Masthead";
+import PortfolioSection from "./PortfolioSection";
+import PortfolioModals from "./PortfolioModals";
+
+const App = () => (
+  <>
+    <Navigation
+      heading = "My Portfolio"
+    />
+    <Masthead
+      heading = "Himanshu Verma" 
+      subheading = {["Software Developer"].join(' - ')}
+    />
+    <PortfolioSection
+      heading = "Portfolio"
+    />
+    <PortfolioModals/>
+  </>
+);
 
 export default App;
