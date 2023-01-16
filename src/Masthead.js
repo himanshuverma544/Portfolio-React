eb8d53a28857f9ffac8ef6ee55e3b7e903835a1b
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-16 15:25:16 +0530

    adding the Masthead component

---
+++ src/Masthead.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Image from "./Image";
+
+import {Divider} from "./RepeatedElements";
+
+const Masthead = ({heading, subheading}) => (
+
+  <header className="masthead bg-primary text-white text-center">
+      <div className="container d-flex align-items-center flex-column">
+          {/* <!-- Masthead Avatar Image--> */}
+          <Image classNameText="masthead-avatar mb-5" name="avataaars.svg" altText="Avatar image of the person"/>
+          {/* <!-- Masthead Heading--> */}
+          <h1 className="masthead-heading text-uppercase mb-0">{heading}</h1>
+          {/* <!-- Icon Divider--> */}
+          <Divider/>
+          {/* <!-- Masthead Subheading--> */}
+          <p className="masthead-subheading font-weight-light mb-0">{subheading}</p>
+      </div>
+  </header>
+ );
+
+ export default Masthead;
