f79df3dbb8700cd83e96c5a98f10cf56bc72c1b3
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-18 13:57:52 +0530

    added the Contact Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-import "./Assets";
-
 import Navigation from "./Navigation";
 import Masthead from "./Masthead";
 import PortfolioSection from "./PortfolioSection";
 import PortfolioModals from "./PortfolioModals";
 import AboutSection from "./AboutSection";
+import ContactSection from "./ContactSection";
+
+import "./Assets";
 
 const App = () => (
   <>
@@ -25,6 +26,37 @@ const App = () => (
       heading = "About"
       theTextContent = {{left : "aboutSecLeftContent", right : "aboutSecRightContent"}}
       link = {{url : "#", title : "Download"}}
+    />
+    <ContactSection
+      heading = "Contact Me"
+      form = {{
+        placeholder : {
+          name : "Enter your name...",
+          email : "name@example.com",
+          phone : "(123) 456-7890",
+          message : "Enter your message here..."
+        },
+        label : {
+          name : "Full Name",
+          email : "Email Address",
+          phone : "Phone Number",
+          message : "Message",
+          btn : "Send"
+        },
+        feedback : {
+          name : "A name is required.",
+          email : {
+            required : "An email is required.",
+            notValid : "Email is not valid"
+          },
+          phone : "A phone number is required.",
+          message : "A message is required."
+        },
+        submissionMsg : {
+          success : "Form submission successful!",
+          error : "Error sending message!"
+        }
+      }}
     />
   </>
 );
