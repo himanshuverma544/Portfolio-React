a36f1101d4426fc878bf5d89e56479ded6d00c65
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-21 23:40:44 +0530

    mod: corrected things for build to run successfully

--- src/Assets.js
+++ src/Assets.js
@@ -1,8 +1,3 @@
-/* Bootstrap core JS */
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js";
-
 /* Core theme JS */
 import "./assets/js/scripts.js";
 
-/* SB Forms JS */
-// import "https://cdn.startbootstrap.com/sb-forms-latest.js";
