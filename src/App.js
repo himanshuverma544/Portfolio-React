7ccbfae5a6da91049d53abcf6db562ee9ef0bcb8
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-28 14:09:21 +0530

    update: added the content in the props for the components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as getKey } from "uuid";
 
 import Navigation from "./Navigation";
 import Masthead from "./Masthead";
@@ -9,6 +10,7 @@ import ContactSection from "./ContactSection";
 import Footer from "./Footer";
 import CopyrightSection from "./CopyrightSection";
 
+import getTextContent from "./TextContent";
 import {getCurrentYear} from "./functions";
 
 
@@ -16,6 +18,11 @@ const App = () => (
   <>
     <Navigation
       heading = "My Portfolio"
+      links = {[
+        {key : getKey(), url : "#portfolio", title : "Portfolio"},
+        {key : getKey(), url : "#about", title : "About"},
+        {key : getKey(), url : "#contact", title : "Contact"}
+      ]}
     />
     <Masthead
       heading = "Himanshu Verma" 
@@ -23,11 +30,79 @@ const App = () => (
     />
     <PortfolioSection
       heading = "Portfolio"
+      pItemData = {[
+        {key : getKey(), theDataBsTargetNumVal : "1", name : "cabin.png", alt : "cabin image"},
+        {key : getKey(), theDataBsTargetNumVal : "2", name : "cake.png", alt : "cake image"},
+        {key : getKey(), theDataBsTargetNumVal : "3", name : "circus.png", alt : "circus image"},
+        {key : getKey(), theDataBsTargetNumVal : "4", name : "game.png", alt : "game image"},
+        {key : getKey(), theDataBsTargetNumVal : "5", name : "safe.png", alt : "safe image"},
+        {key : getKey(), theDataBsTargetNumVal : "6", name : "submarine.png", alt : "submarine image"}
+      ]}
     />
-    <PortfolioModals/>
+    <PortfolioModals
+      pModalData = {[
+        {
+          key : getKey(),
+          numValue : "1",
+          heading : "Log Cabin",
+          img : {name : "cabin.png", alt : "Cabin Image"},
+          theTextContent : getTextContent("porCabinModal"),
+          btnText : "Close Window"
+        },
+        {
+          key : getKey(),
+          numValue : "2",
+          heading : "Tasty Cake",
+          img : {name : "cake.png", alt : "Cake Image"},
+          theTextContent : getTextContent("porCakeModal"),
+          btnText : "Close Window"
+        },
+        {
+          key : getKey(),
+          numValue : "3",
+          heading : "Circus Tent",
+          img : {name : "circus.png", alt : "Circus Image"},
+          theTextContent : getTextContent("porCircusModal"),
+          btnText : "Close Window"
+        },
+        {
+          key : getKey(),
+          numValue : "4",
+          heading : "Controller",
+          img : {name : "game.png", alt : "Game Image"},
+          theTextContent : getTextContent("porGameModal"),
+          btnText : "Close Window"
+        },
+        {
+          key : getKey(),
+          numValue : "5",
+          heading : "Locked Safe",
+          img : {name : "safe.png", alt : "Safe Image"},
+          theTextContent : getTextContent("porSafeModal"),
+          btnText : "Close Window"
+        },
+        {
+          key : getKey(),
+          numValue : "6",
+          heading : "Submarine",
+          img : {name : "submarine.png", alt : "Submarine Image"},
+          theTextContent : getTextContent("porSubmarineModal"),
+          btnText : "Close Window"
+        }
+      ]}
+    />
     <AboutSection
       heading = "About"
-      theTextContent = {{left : "aboutSecLeftContent", right : "aboutSecRightContent"}}
+      textContentData = {[{
+        key : getKey(),
+        classNameText : "col-lg-4 ms-auto",
+        theTextContent : getTextContent("aboutSecLeftContent")
+      },
+      {
+        key : getKey(),
+        classNameText : "col-lg-4 me-auto",
+        theTextContent : getTextContent("aboutSecRightContent")
+      }]}
       link = {{url : "#", title : "Download"}}
     />
     <ContactSection
@@ -68,28 +143,16 @@ const App = () => (
       }}
       centerContent = {{
         heading : "Around the Web",
-        social : {
-          facebook : {
-            link : "#",
-            icon : "facebook-f"
-          },
-          twitter : {
-            link : "#",
-            icon : "twitter",
-          },
-          linkedin : {
-            link : "#",
-            icon : "linkedin-in",
-          },
-          dribbble : {
-            link : "#",
-            icon : "dribbble"
-          }
-        }
+        social : [
+          {key : getKey(), link : "#", icon : "facebook-f"},
+          {key : getKey(), link : "#", icon : "twitter"},
+          {key : getKey(), link : "#", icon : "linkedin-in"},
+          {key : getKey(), link : "#", icon : "dribbble"}
+        ]  
       }}
       rightContent = {{
         heading : "About",
-        theTextContent : "footRightConAbout"
+        theTextContent : getTextContent("footRightConAbout")
       }}
     />
     <CopyrightSection textContent = {`Copyright © ${getCurrentYear()} by My Portfolio. All Rights Reserved.`}/>
