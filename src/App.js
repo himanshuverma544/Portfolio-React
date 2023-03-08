a48743a8a32c59fb017870401b603bb6753ea675
Himanshu Verma <himanshuverma544@gmail.com>
2023-03-08 20:55:57 +0530

    update: removed the section

--- src/App.js
+++ src/App.js
@@ -106,37 +106,6 @@ const App = () => (
       }]}
       link = {{url : "#", title : "Download"}}
     />
-    <ContactSection
-      heading = "Contact Me"
-      form = {{
-        placeholder : {
-          name : "Enter your name...",
-          email : "name@example.com",
-          phone : "(123) 456-7890",
-          message : "Enter your message here..."
-        },
-        label : {
-          name : "Full Name",
-          email : "Email Address",
-          phone : "Phone Number",
-          message : "Message",
-          btn : "Send"
-        },
-        feedback : {
-          name : "A name is required.",
-          email : {
-            required : "An email is required.",
-            notValid : "Email is not valid"
-          },
-          phone : "A phone number is required.",
-          message : "A message is required."
-        },
-        submissionMsg : {
-          success : "Form submission successful!",
-          error : "Error sending message!"
-        }
-      }}
-    />
     <Footer
       leftContent = {{
         locHeading : "Location",
