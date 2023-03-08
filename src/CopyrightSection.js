bc85011ee08b3ed00bba8ebe0d947bfb1ed2da87
Himanshu Verma <himanshuverma544@gmail.com>
2023-03-08 20:37:49 +0530

    del: removed the files from the current folder

--- src/CopyrightSection.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CopyrightSection = ({textContent}) => (
-  <div className="copyright py-4 text-center text-white">
-    <div className="container"><small>{textContent}</small></div>
-  </div>
-);
-
-export default CopyrightSection;
